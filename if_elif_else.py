021127df823f0b28b09716e14a48d72abe6d18e8
Chad Yarborough <chad.m.yarborough@icloud.com>
2026-04-19 09:21:49 -0700

    Refactor conditional statements in if_elif_else.py for clarity

--- if_elif_else.py
+++ if_elif_else.py
@@ -3,16 +3,18 @@ if x > 3:
     print("x is greater than 3")
 elif x == 1:
     print("x is 1")
-else:    print("x is less than or equal to 3 and not 1")
+else:    print("x is equal to: ", x)
+
 x = x + 1
 if x > 3:
     print("x is greater than 3")
 elif x == 1:
     print("x is 1")
-else:    print("x is less than or equal to 3 and not 1")
+else:    print("x is equal to: ", x)
+
 x = x + 1
 if x > 3:
     print("x is greater than 3")
-elif x == 1:
+elif x == 1:        
     print("x is 1")
-else:    print("x is less than or equal to 3 and not 1")
+else:    print("x is equal to: ", x)
