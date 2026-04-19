29461b4513f7621c1a8a63f81b6881fe3a4642ea
Chad Yarborough <chad.m.yarborough@icloud.com>
2026-04-19 09:17:11 -0700

    Add if_else_elif.py and while_statement.py for conditional and loop demonstrations

---
+++ if_else_elif.py
@@ -0,0 +1,18 @@
+x = 1
+if x > 3:
+    print("x is greater than 3")
+elif x == 1:
+    print("x is 1")
+else:    print("x is less than or equal to 3 and not 1")
+x = x + 1
+if x > 3:
+    print("x is greater than 3")
+elif x == 1:
+    print("x is 1")
+else:    print("x is less than or equal to 3 and not 1")
+x = x + 1
+if x > 3:
+    print("x is greater than 3")
+elif x == 1:
+    print("x is 1")
+else:    print("x is less than or equal to 3 and not 1")
